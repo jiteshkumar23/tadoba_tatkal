2e56830b381b699272611ece3c9b4c6af08c48c2
jiteshkumar23 <jitesh.f5@gmail.com>
2024-02-04 18:20:03 +0530

    major update with autofill plugin
    major update with autofill plugin

--- src/main/java/booking/TatkalBooking.java
+++ src/main/java/booking/TatkalBooking.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,8 +17,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.sikuli.script.FindFailed;
-import org.sikuli.script.Pattern;
-import org.sikuli.script.Screen;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -42,12 +42,10 @@ public class TatkalBooking extends TatkalBooking_DataProfile1 {
 	static ArrayList<String> listOfChildName;
 	static ArrayList<String> listOfChildAge;
 	static ArrayList<String> listOfCameraSelection;
-	static int maxCount;
 	static String numberOfMembers;
 	static int intNumberOfMembers;
 	static String dateToBeClickedOnCalendar;
 	static Actions actions;
-	static String maxCountFromLink;
 	static String xpathForLink;
 
 	@BeforeTest()
@@ -73,72 +71,9 @@ public class TatkalBooking extends TatkalBooking_DataProfile1 {
 		wait200 = new WebDriverWait(driver, Duration.ofSeconds(200));
 		actions = new Actions(driver);
 
-		listOfName = new ArrayList<String>();
-		listOfName.add(TatkalBooking_DataProfile1.name1FromData);
-		listOfName.add(TatkalBooking_DataProfile1.name2FromData);
-		listOfName.add(TatkalBooking_DataProfile1.name3FromData);
-		listOfName.add(TatkalBooking_DataProfile1.name4FromData);
-		listOfName.add(TatkalBooking_DataProfile1.name5FromData);
-		listOfName.add(TatkalBooking_DataProfile1.name6FromData);
-
-		listOfGender = new ArrayList<String>();
-		listOfGender.add(TatkalBooking_DataProfile1.gender1FromData);
-		listOfGender.add(TatkalBooking_DataProfile1.gender2FromData);
-		listOfGender.add(TatkalBooking_DataProfile1.gender3FromData);
-		listOfGender.add(TatkalBooking_DataProfile1.gender4FromData);
-		listOfGender.add(TatkalBooking_DataProfile1.gender5FromData);
-		listOfGender.add(TatkalBooking_DataProfile1.gender6FromData);
-
-		listOfAge = new ArrayList<String>();
-		listOfAge.add(TatkalBooking_DataProfile1.age1FromData);
-		listOfAge.add(TatkalBooking_DataProfile1.age2FromData);
-		listOfAge.add(TatkalBooking_DataProfile1.age3FromData);
-		listOfAge.add(TatkalBooking_DataProfile1.age4FromData);
-		listOfAge.add(TatkalBooking_DataProfile1.age5FromData);
-		listOfAge.add(TatkalBooking_DataProfile1.age6FromData);
-
-		listOfCountry = new ArrayList<String>();
-		listOfCountry.add(TatkalBooking_DataProfile1.country1FromData);
-		listOfCountry.add(TatkalBooking_DataProfile1.country2FromData);
-		listOfCountry.add(TatkalBooking_DataProfile1.country3FromData);
-		listOfCountry.add(TatkalBooking_DataProfile1.country4FromData);
-		listOfCountry.add(TatkalBooking_DataProfile1.country5FromData);
-		listOfCountry.add(TatkalBooking_DataProfile1.country6FromData);
-
-		listOfId = new ArrayList<String>();
-		listOfId.add(TatkalBooking_DataProfile1.Id1FromData);
-		listOfId.add(TatkalBooking_DataProfile1.Id2FromData);
-		listOfId.add(TatkalBooking_DataProfile1.Id3FromData);
-		listOfId.add(TatkalBooking_DataProfile1.Id4FromData);
-		listOfId.add(TatkalBooking_DataProfile1.Id5FromData);
-		listOfId.add(TatkalBooking_DataProfile1.Id6FromData);
-
-		listOfIdNumber = new ArrayList<String>();
-		listOfIdNumber.add(TatkalBooking_DataProfile1.IdNumber1FromData);
-		listOfIdNumber.add(TatkalBooking_DataProfile1.IdNumber2FromData);
-		listOfIdNumber.add(TatkalBooking_DataProfile1.IdNumber3FromData);
-		listOfIdNumber.add(TatkalBooking_DataProfile1.IdNumber4FromData);
-		listOfIdNumber.add(TatkalBooking_DataProfile1.IdNumber5FromData);
-		listOfIdNumber.add(TatkalBooking_DataProfile1.IdNumber6FromData);
-
 		numberOfMembers = TatkalBooking_DataProfile1.numberOfMembers;
 		intNumberOfMembers = Integer.parseInt(numberOfMembers);
 
-		listOfChildName = new ArrayList<String>();
-		listOfChildName.add(TatkalBooking_DataProfile1.child1Name);
-		listOfChildName.add(TatkalBooking_DataProfile1.child2Name);
-
-		listOfChildAge = new ArrayList<String>();
-		listOfChildAge.add(TatkalBooking_DataProfile1.child1Age);
-		listOfChildAge.add(TatkalBooking_DataProfile1.child2Age);
-
-		listOfCameraSelection = new ArrayList<String>();
-		listOfCameraSelection.add(TatkalBooking_DataProfile1.camera1);
-		listOfCameraSelection.add(TatkalBooking_DataProfile1.camera2);
-		listOfCameraSelection.add(TatkalBooking_DataProfile1.camera3);
-		listOfCameraSelection.add(TatkalBooking_DataProfile1.camera4);
-		listOfCameraSelection.add(TatkalBooking_DataProfile1.camera5);
-		listOfCameraSelection.add(TatkalBooking_DataProfile1.camera6);
 	}
 
 	@Test
@@ -193,15 +128,8 @@ public class TatkalBooking extends TatkalBooking_DataProfile1 {
 		// select number of people in dropdown
 		selectNumberOfPeopleInDropdown();
 
-		// enter Member details
-
-		fillMemberDetailsTadoba();
-
-		fillChildrenDetails();
-
-		fillTouristAddressAndMobile();
-
-		enterCameraDetails();
+		// enter Member details from Autofill plugin
+		fillMemberDetailsfromAutoFillPlugin();
 
 		selectAgreement();
 
@@ -220,28 +148,48 @@ public class TatkalBooking extends TatkalBooking_DataProfile1 {
 
 	}
 
+	private static void fillMemberDetailsfromAutoFillPlugin() {
+		// WebElement element =
+		// driver.findElement(By.xpath("//h1[normalize-space()='Vehicle Booking']"));
+		WebElement bodyElement = driver.findElement(By.tagName("body"));
+		actions.keyDown(Keys.ALT).sendKeys("q").keyUp(Keys.ALT).build().perform();
+		actions.moveToElement(bodyElement).perform();
+
+	}
+
 	private static void clickOnLinkForRegularFlow() throws InterruptedException {
 		wait15.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//tbody/tr[3]//a)[1]")));
 		WebElement availableOption = driver.findElement(By.xpath("(//tbody/tr[3]//a)[1]"));
-		maxCountFromLink = driver.findElement(By.xpath("(//tbody/tr[3]//a)[1]")).getText();
-		System.out.println("maxCountFromLink is ---> " + maxCountFromLink);
 		Thread.sleep(250);
 		wait15.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("(//tbody/tr[3]//a)[1]"))));
 		driver.findElement(By.xpath("(//tbody/tr[3]//a)[1]")).click();
+		System.out.println("###################   Link Clicked   ###################");
+		System.out.println("");
+		printDateTime("Link Clicked Time -->");
 
 	}
 
 	private static void clickOnLinkForTatkalFlow() throws InterruptedException {
-		// xpath morning - //tbody/tr[3]/td[6]/a[1]
-		// xpath afternoon - //tbody/tr[3]/td[7]/a[1]
 
 		if (shiftFromData.toLowerCase().equalsIgnoreCase("morning")) {
 
-			xpathForLink = "//tbody/tr[3]/td[6]/a[1]";
+			if (gateFromData.toLowerCase().equalsIgnoreCase("Zari")) {
+
+				xpathForLink = "//tbody/tr[4]/td[6]/a[1]";
+
+			} else {
+				xpathForLink = "//tbody/tr[3]/td[6]/a[1]";
+			}
 
 		} else if (shiftFromData.toLowerCase().equalsIgnoreCase("afternoon")) {
 
-			xpathForLink = "//tbody/tr[3]/td[7]/a[1]";
+			if (gateFromData.toLowerCase().equalsIgnoreCase("Zari")) {
+
+				xpathForLink = "//tbody/tr[4]/td[7]/a[1]";
+			} else {
+				xpathForLink = "//tbody/tr[3]/td[7]/a[1]";
+			}
+
 		}
 		System.out.println("Now attempting to check if link is displayed or not ");
 		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(250));
@@ -260,11 +208,12 @@ public class TatkalBooking extends TatkalBooking_DataProfile1 {
 		try {
 			wait15.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(xpathForLink))));
 			wait15.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath(xpathForLink))));
-			maxCountFromLink = driver.findElement(By.xpath(xpathForLink)).getText();
-			System.out.println("maxCountFromLink is ---> " + maxCountFromLink);
 			Thread.sleep(250);
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 			driver.findElement(By.xpath(xpathForLink)).click();
+			System.out.println("###################   Link Clicked   ###################");
+			System.out.println("");
+			printDateTime("Link Clicked Time -->");
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
@@ -368,161 +317,24 @@ public class TatkalBooking extends TatkalBooking_DataProfile1 {
 		}
 	}
 
-	private static void selectAgreement() {
+	private static void selectAgreement() throws InterruptedException {
 
 		WebElement agreementCheckBox = driver.findElement(By.xpath("//input[@id='agree_term']"));
 		scrollToElementUsingMouse(driver, agreementCheckBox);
+		Thread.sleep(200);
 		agreementCheckBox.click();
 		wait15.until(ExpectedConditions.alertIsPresent());
 		driver.switchTo().alert().accept();
 		try {
-		wait10.until(ExpectedConditions
-				.textToBePresentInElementLocated(By.xpath("//th[normalize-space()='Grand Total']"), "Grand Total"));
-		}catch(Exception e) {
+			wait10.until(ExpectedConditions
+					.textToBePresentInElementLocated(By.xpath("//th[normalize-space()='Grand Total']"), "Grand Total"));
+		} catch (Exception e) {
 			System.out.println("Grand total was not found");
 		}
 		scrollToElementUsingMouse(driver, driver.findElement(By.xpath("//button[@id='btnSubmit']")));
+		Thread.sleep(100);
 		scrollDownUsingMouse(driver, 5);
-	}
-
-	private static void enterCameraDetails() {
-		if (cameraRequired.toLowerCase().equalsIgnoreCase("yes")) {
-
-			WebElement yesRadio = driver.findElement(By.xpath("(//input[@id='your_camera1'])[1]"));
-			yesRadio.click();
-
-			WebElement cameraDropDown = driver.findElement(By.xpath("//select[@id='no_of_camera']"));
-			wait15.until(ExpectedConditions.elementToBeClickable(cameraDropDown));
-			Select select4 = new Select(driver.findElement(By.xpath("//select[@id='no_of_camera']")));
-			select4.selectByVisibleText(numberOfCamera);
-
-			int intNumberOfCamera = Integer.parseInt(numberOfCamera);
-			for (int i = 0; i < intNumberOfCamera; i++) {
-
-				String cameraXpath = "(//select[@id='cmbCamera";
-				WebElement cameraDropDownElement = driver.findElement(By.xpath(cameraXpath + i + "'])[1]"));
-				wait15.until(ExpectedConditions.elementToBeClickable(cameraDropDownElement));
-				Select select4A = new Select(driver.findElement(By.xpath(cameraXpath + i + "'])[1]")));
-				if (listOfCameraSelection.get(i).equalsIgnoreCase("PointAndShoot")) {
-					select4A.selectByIndex(1);
-				} else if (listOfCameraSelection.get(i).equalsIgnoreCase("VideoCamera")) {
-					select4A.selectByIndex(2);
-				}
-
-			}
-
-		} else if (cameraRequired.toLowerCase().equalsIgnoreCase("no")) {
-			WebElement noRadio = driver.findElement(By.xpath("(//input[@id='your_camera1'])[2]"));
-			noRadio.click();
-		}
-
-	}
-
-	private static void fillTouristAddressAndMobile() {
-
-		WebElement touristAdd = driver.findElement(By.xpath("//textarea[@id='address']"));
-		touristAdd.clear();
-		touristAdd.sendKeys(touristAddress);
-
-		WebElement touristMob = driver.findElement(By.xpath("//input[@id='mobile_no']"));
-		touristMob.clear();
-		touristMob.sendKeys(touristMobileNumber);
-
-	}
-
-	private static void fillChildrenDetails() {
-		int intnumberofChildren = Integer.parseInt(numberOfChildren);
-		if (intnumberofChildren > 0) {
-
-			for (int i = 1; i <= intnumberofChildren; i++) {
-				// enter name
-				String basicXpathChildName = "(//input[@id='children_name";
-				WebElement childName = driver.findElement(By.xpath(basicXpathChildName + i + "'])[1]"));
-				childName.sendKeys(listOfChildName.get(i - 1));
-
-				// enter age
-
-				String childXpathAge = "(//select[@id='child_age";
-				WebElement ageDropDown = driver.findElement(By.xpath(childXpathAge + i + "'])[1]"));
-				wait15.until(ExpectedConditions.elementToBeClickable(ageDropDown));
-				Select select4 = new Select(driver.findElement(By.xpath(childXpathAge + i + "'])[1]")));
-				select4.selectByVisibleText(listOfChildAge.get(i - 1));
-
-			}
-		}
-
-	}
-
-	private static void fillMemberDetailsTadoba() throws FindFailed, InterruptedException {
-		for (int i = 0; i < maxCount; i++) {
-
-			// fill name
-			String basicXpathName = "//input[@id='touristName";
-			WebElement touristName = driver.findElement(By.xpath(basicXpathName + i + "']"));
-			touristName.clear();
-			touristName.sendKeys(listOfName.get(i));
-
-			// fill gender
-			String gender = listOfGender.get(i);
-
-			switch (gender) {
-			case "Male": {
-				String basicXpathMaleGender = "(//input[@name='gender";
-				WebElement maleGenderRadio = driver.findElement(By.xpath(basicXpathMaleGender + i + "'])[1]"));
-				maleGenderRadio.click();
-			}
-				break;
-			case "Female": {
-				String basicXpathFemaleGender = "(//input[@name='gender";
-				WebElement femaleGenderRadio = driver.findElement(By.xpath(basicXpathFemaleGender + i + "'])[2]"));
-				femaleGenderRadio.click();
-			}
-				break;
-			case "Transgender": {
-				String basicXpathTransgender = "(//input[@name='gender";
-				WebElement transGenderRadio = driver.findElement(By.xpath(basicXpathTransgender + i + "'])[3]"));
-				transGenderRadio.click();
-			}
-				break;
-			default: {
-				String basicXpathMaleGender = "(//input[@name='gender";
-				WebElement maleGenderRadio = driver.findElement(By.xpath(basicXpathMaleGender + i + "'])[1]"));
-				maleGenderRadio.click();
-			}
-				break;
-			}
-
-			// select Age
-			String basicXpathAge = "(//select[@id='age";
-			WebElement ageDropDown = driver.findElement(By.xpath(basicXpathAge + i + "'])[1]"));
-			wait15.until(ExpectedConditions.elementToBeClickable(ageDropDown));
-			Select select4 = new Select(driver.findElement(By.xpath(basicXpathAge + i + "'])[1]")));
-			select4.selectByVisibleText(listOfAge.get(i));
-
-			// select Country
-
-			String basicXpathCountry = "(//select[@id='cmbCountry";
-			WebElement countryDropDown = driver.findElement(By.xpath(basicXpathCountry + i + "'])[1]"));
-			wait15.until(ExpectedConditions.elementToBeClickable(countryDropDown));
-			Select select5 = new Select(driver.findElement(By.xpath(basicXpathCountry + i + "'])[1]")));
-			select5.selectByVisibleText(listOfCountry.get(i));
-
-			// select Identity
-
-			String basicXpathIdentity = "(//select[@id='cmbIndentity";
-			WebElement identityDropDown = driver.findElement(By.xpath(basicXpathIdentity + i + "'])[1]"));
-			wait15.until(ExpectedConditions.elementToBeClickable(identityDropDown));
-			Select select6 = new Select(driver.findElement(By.xpath(basicXpathIdentity + i + "'])[1]")));
-			select6.selectByVisibleText(listOfId.get(i));
-
-			// enter identitiy number
-
-			String basicXpathIdentityNumber = "(//input[@id='indentityNo";
-			WebElement identityNumberField = driver.findElement(By.xpath(basicXpathIdentityNumber + i + "'])[1]"));
-			wait15.until(ExpectedConditions.elementToBeClickable(identityNumberField));
-			identityNumberField.sendKeys(listOfIdNumber.get(i));
-		}
-
+		Thread.sleep(200);
 	}
 
 	private static void selectNumberOfPeopleInDropdown() {
@@ -533,12 +345,15 @@ public class TatkalBooking extends TatkalBooking_DataProfile1 {
 		Select select4 = new Select(driver.findElement(By.xpath("//select[@id='no_people']")));
 
 		try {
-			select4.selectByVisibleText(String.valueOf(maxCount));
+			select4.selectByVisibleText(String.valueOf(intNumberOfMembers));
 		} catch (Exception e) {
 
 			e.printStackTrace();
 			select4.selectByVisibleText(numberOfMembers);
 		}
+
+		wait15.until(
+				ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//input[@id='touristName0']"))));
 
 	}
 
@@ -603,15 +418,7 @@ public class TatkalBooking extends TatkalBooking_DataProfile1 {
 	}
 
 	public static void setMaxCount() {
-		System.out.println(maxCountFromLink);
-		maxCount = Integer.parseInt(maxCountFromLink);
-		if (intNumberOfMembers < maxCount) {
-			maxCount = intNumberOfMembers;
-		}
-		if (maxCount > 6) {
-			maxCount = 6;
-		}
-		System.out.println("Updated max count is " + maxCount);
+		System.out.println("Number of people to select is " + intNumberOfMembers);
 	}
 
 	public static void selectVehicleForRegularFlow() throws InterruptedException {
